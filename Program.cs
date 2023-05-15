4e7fc257dd6032d018fd86b3dad26128d32c27bc
Your Name <you@example.com>
2023-05-15 16:27:48 +0700

    Tunknum1

--- Program.cs
+++ Program.cs
@@ -4,58 +4,34 @@
        float max = float.Parse(Console.ReadLine());
 
         Console.Write("Input Vfill: ");
-       float drink = float.Parse(Console.ReadLine());
-
-        Console.Write("Input Vdrink ");
        float fill = float.Parse(Console.ReadLine());
 
+        Console.Write("Input Vdrink ");
+       float drink = float.Parse(Console.ReadLine());
+
         Console.Write("Input Tfill: ");
-       float tdrink = float.Parse(Console.ReadLine());
+       float tfill = float.Parse(Console.ReadLine());
 
         Console.Write("Input Tdrink: ");
-       float tfill = float.Parse(Console.ReadLine());
+       float tdrink = float.Parse(Console.ReadLine());
 
         Console.Write("Input Tday: ");
        float tday = float.Parse(Console.ReadLine());
-float V =0;
-bool enoughWater = true;
+        v = (drink*tfill)-(fill*tfill);
 
-for (int t = 0; t < tday; t++)
-        {
-            if (t % (tdrink + tfill) < tdrink)
-            {
-                V += drink;
-            }
-            else
-            {
-                V -= fill;
-            }
-
-            if (V > max)
-            {
-                Console.WriteLine("Overflow Water");
-                enoughWater = false;
-                break;
-            }
-        }
-
-        if (enoughWater)
-        {
-            if (V >= 0)
-            {
-                Console.WriteLine("Enough Water, {0} left", V);
-            }
-            else
-            {
-                Console.WriteLine("Not Enough Water");
-            }
+if(v>=0){
+            Console.WriteLine("Enough Water {0} left",v);
+        if(tday%drink>=0){
+            Console.WriteLine("Overflow Water");}        }  
+        
+else{
+        Console.WriteLine("Not Enough Water");
+        if(tday%drink>=0){
+            Console.WriteLine("Overflow Water");
         }
     }
 }
-
-
-
-
+}
 
     
 
